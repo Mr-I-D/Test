772b5b69cca9d7395eeaf7809327771b04b286f3
Mr-I-D <148846699+Mr-I-D@users.noreply.github.com>
2024-04-06 21:05:48 +0300

    mission complited

--- obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70560cc6b15cc5d287a4040f1fd6d98bdc0376a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+517322309cb6c8f2891dd83b6294d7e1665831b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
